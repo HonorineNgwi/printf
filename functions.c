17e30a038274ab3c71acbefd4eb5a1b3e48f3e4c
prisco01 <dapo.olagunju@gmail.com>
2022-11-24 06:04:06 -0800

    update

--- functions.c
+++ functions.c
@@ -10,7 +10,7 @@
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
- *   */
+ */
 int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
@@ -28,7 +28,7 @@ int print_char(va_list types, char buffer[],
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
- *    */
+ */
 int print_string(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
